38d3598605659d947059047f36204869a4a8ec49
giorgi725 <megrelig11@gmail.com>
2025-10-11 16:22:34 +0400

    commit

---
+++ day 005/homework/homework1.py
@@ -0,0 +1,7 @@
+# მომხმარებელს შემოატანინეთ თავისი სახელი, გვარი, ასაკი და ქალაქი. შემდეგ ეს ინფორმაცია ერთ წინადადებად დაუბეჭდეთ ტერმინალში
+
+name = input("Enter your name: ")
+surname = input("Enter your surname: ")
+age = input("Enter your age: ")
+city = input("Enter your city: ")
+print(f"Hello, {name} {surname} you are {age} years old and live in {city}")
---
+++ day 005/homework/homework3.py
@@ -0,0 +1,43 @@
+#თითოეულ მათემატიკურ ოპერატორზე (+, -, *, /, %, **, //) შეასრულეთ 5 მაგალითი და დაბეჭდეთ შედეგი
+
+print(10 + 2)
+print(15 + 3)
+print(20 + 70)
+print(40 + 45)
+print(50 + 90)
+
+print(40 - 24)
+print(55 - 34)
+print(77 - 76)
+print(99 - 34)
+print(45 - 12)
+
+print(12 * 4)
+print(15 * 7)
+print(28 * 9)
+print(99 * 5)
+print(34 * 8)
+
+print(44 / 2)
+print(55 / 5)
+print(35 /7)
+print(21 / 3)
+print(91 / 9)
+
+print(34 % 6)
+print(89 % 9)
+print(99 % 8)
+print(21 % 4)
+print(55 % 32)
+
+print(89 ** 2)
+print(56 ** 4)
+print(87 ** 8)
+print(23 ** 5)
+print(76 ** 3)
+
+print(25 // 5)
+print(45 // 9)
+print(78 // 2)
+print(93 // 3)
+print(18 // 6)
