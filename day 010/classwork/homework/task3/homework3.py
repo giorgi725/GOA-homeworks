ceaab9874a3b571059f38b05456be7a6fb15760f
giorgi725 <megrelig11@gmail.com>
2025-10-20 23:30:20 +0400

    commit

---
+++ day 010/classwork/homework/task3/homework3.py
@@ -0,0 +1,49 @@
+#4) შექმენით 30 მაგალითი შედარების ოპერაციებზე (>, <, >=, <=, ==, !=), გამოიყენეთ სხვადასხვა ტიპის მნიშვნელობები (რიცხვები, სტრინგები, ცვლადები). თითოეულ მაგალითში კომენტარით მიუწერეთ, რას აბრუნებს (True ან False) და რატომ მაგალითად, თუ ობიექტები განსხვავებული ტიპისაა, ახსენით როგორ იქნა შედარებული. ეცადეთ, რომ მაგალითები არ გამეორდეს და მოიცავდეს სხვადასხვა სიტუაციებს (მათ შორის ცვლადების გამოყენებასაც)
+
+
+name = "gio"
+print(name == "luka") # false
+num1 = 4
+print(num1 > 3) # true
+num2 = 3.14
+print(num2 < 3.50) # true 
+surname = "smith"
+print(surname == "smith") # true
+num4 = 39
+print(num4 >= 43) # false
+num5 = 54
+print(num5 <= 33) # false
+num6 = 34
+print(num6 > 25) # true
+num7 = 53
+print(num7 < 45) # false
+num8 = 55
+print(num8 == 55) # true
+num9 = 37
+print(num9 <= 78) # true
+num10 = 64
+print(num10 >= 69) # false
+num11 = 24
+print(num11 != 23) # true
+num12 = 75
+print(num12 == 58) # false
+number_1 = 12
+number_2 = 34
+print(number_2 > number_1) # true
+print('gio' != 'gio') # false
+print("apple" == "apple") # True  
+print("Apple" == "apple") # False 
+print("cat" != "dog") # True  
+print("a" < "b") # True     
+print("hi" < "hello") # false
+print(10 > 3)# True 
+print(7 <= 7) # True 
+print(5 != 4) # True
+print(3.5 < 4.0) # True   
+print(2.0 == 2) # True  
+print("dog" != "cat") # True
+print("apple" < "banana") # True 
+print("Hello" == "hello") # False 
+print("zoo" > "zoom") # False 
+print("Python" >= "Java") # True 
+print("CsS" == "CSS") # false
