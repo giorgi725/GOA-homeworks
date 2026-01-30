2e7b754e46451a0ba91f7cb2fd7c6254ac7e3c1e
giorgi725 <megrelig11@gmail.com>
2026-01-30 14:48:02 +0400

    commit

--- day 052/homework/codewars.py
+++ day 052/homework/codewars.py
@@ -161,7 +161,7 @@ def is_valid_walk(walk):
 """https://www.codewars.com/kata/545cedaa9943f7fe7b000048/train/python"""
 
 #Detect Pangram
-
+#
 def is_pangram(st):
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     st = st.lower()
